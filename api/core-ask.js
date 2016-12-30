c63732ca68070d75f8164bbb6442af588f16a54b
Patrick Quinn <patrickquinn@Patricks-MacBook-Air-2.local>
2016-12-30 11:58:04 +0000

    Added better command parsing

--- api/core-ask.js
+++ api/core-ask.js
@@ -10,18 +10,87 @@ function _query(q) {
   classifier.addDocument('is it raining', 'weather');
   classifier.addDocument('is it cold outside', 'weather');
   classifier.addDocument('what is the weather', 'weather');
+  classifier.addDocument('whats the weather', 'weather');
+  classifier.addDocument('whats it like outside', 'weather');
+  classifier.addDocument('whats it like in', 'weather');
+
+
 
   // Time train
   classifier.addDocument('what time is it', 'time');
   classifier.addDocument('what day is it', 'time');
   classifier.addDocument('monday tuesday wednesday thursday friday', 'time');
 
-  // Time train
+
+  // Year train
+  classifier.addDocument('what year is it', 'year');
+  classifier.addDocument('what is the year', 'year');
+
+  // Location train
+  classifier.addDocument('where am i', 'location');
+  classifier.addDocument('whats my location', 'location');
+
+  // Sport train
   classifier.addDocument('who won the game', 'sports');
   classifier.addDocument('who is playing', 'sports');
   classifier.addDocument('who is winning', 'sports');
   classifier.addDocument('what is the score', 'sports');
   classifier.addDocument('sports', 'sports');
+
+  // Media play train 
+  classifier.addDocument('play song', 'play');
+  classifier.addDocument('play', 'play');
+  classifier.addDocument('start song', 'play');
+
+  // Media pause train
+  classifier.addDocument('play song', 'play');
+  classifier.addDocument('play', 'play');
+  classifier.addDocument('start song', 'play');
+
+  // Media next train
+  classifier.addDocument('play next', 'next');
+  classifier.addDocument('play next song', 'next');
+  classifier.addDocument('next song', 'nex');
+
+  // Media song train
+  classifier.addDocument('play a song', 'song');
+  classifier.addDocument('play me a song', 'song');
+  classifier.addDocument('start some music', 'song');
+
+  // ID train
+  classifier.addDocument('who am i', 'id');
+  classifier.addDocument('whats my name', 'id');
+  classifier.addDocument('where do i live', 'id');
+  classifier.addDocument('where do i work', 'id');
+  classifier.addDocument('what age am i', 'id');
+  classifier.addDocument('whats my mothers name', 'id');
+  classifier.addDocument('whats my fathers name', 'id');
+
+  // Laughter train
+  classifier.addDocument('im feeling sad', 'joke');
+  classifier.addDocument('im feeling blue', 'joke');
+  classifier.addDocument('im feeling down', 'joke');
+  classifier.addDocument('tell a joke', 'joke');
+  classifier.addDocument('tell me something funny', 'joke');
+  classifier.addDocument('tell me a joke', 'joke');
+  classifier.addDocument('make me laugh', 'joke');
+
+  // Fact train
+  classifier.addDocument('what is', 'fact');
+  classifier.addDocument('who is', 'fact');
+  classifier.addDocument('when is', 'fact');
+  classifier.addDocument('how old is', 'fact');
+  classifier.addDocument('who is in', 'fact');
+
+  // Lang train
+  classifier.addDocument('spell', 'lang');
+  classifier.addDocument('what is the definition of', 'fact');
+  classifier.addDocument('pronounce', 'fact');
+
+  // News train
+  classifier.addDocument('spell', 'lang');
+  classifier.addDocument('what is the definition of', 'fact');
+  classifier.addDocument('pronounce', 'fact');
 
   classifier.train();
 
