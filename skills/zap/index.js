56d1ca8ceb71820959a92d6e8adc76b136e7a333
Patrick Quinn <patrick.jm.quinn@gmail.com>
2017-02-06 11:28:59 +0000

    Clean up and refactored zap skill

--- skills/zap/index.js
+++ skills/zap/index.js
@@ -1,8 +1,8 @@
-const http = require('http');
+const http = require('http')
 
-const config = require('../../config');
-const keys = config.get;
-const services = require('./services.json');
+const config = require('../../config')
+const keys = config.get
+const services = require('./services.json')
 
 const intent = () => ({
     keywords: ['switch to', 'switch to qqqq'],
@@ -10,43 +10,44 @@ const intent = () => ({
 })
 
 function * zap_resp(query) {
-    const channel = query.split('to ')[1];	
-	var ref = getReference(channel);
-		
-	var isZapped = zap(ref);	
-				
-	if(isZapped){
-		return 'Zapped';
-	}
-	
+    const channel = query.split('to ')[1]
+    const ref = getReference(channel)
+
+    const isZapped = zap(ref)
+
+    if (isZapped) {
+        return 'Zapped'
+    }
 }
 
 function zap(ref) {
-	var state;
+    let state
 
-	var options = {
-		host: keys.zap.ip,
-		path: '/api/zap?sRef='+ref
-	};
+    const options = {
+        host: keys.zap.ip,
+        path: '/api/zap?sRef=' + ref
+    }
 
-	callback = 
+    callback =
 	function (response) {
-		var str = '';
-		response.on('data', function (chunk) {str += chunk;});
-		response.on('end', function () {								
-								var parsed = JSON.parse(str);
-								state = parsed.result;
-							});
-	}
-	
-	http.request(options, callback).end();
-	
-	console.log(state);
-	return state;	
+    let str = ''
+    response.on('data', chunk => {
+        str += chunk
+    })
+    response.on('end', () => {
+        const parsed = JSON.parse(str)
+        state = parsed.result
+    })
 }
 
-function getReference(channel){
-	return services[channel];
+    http.request(options, callback).end()
+
+    console.log(state)
+    return state
+}
+
+function getReference(channel) {
+    return services[channel]
 }
 
 module.exports = {
