144675889a42fe18667a0ccd8c2b414924bed024
DevelopingUnicorn <snimaa11@htlkaindorf.at>
2017-02-06 12:19:48 +0100

    Added zap ip adress option to config

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
-var config = {
-	openweathermap: {key:'f08bfa92a064d679ae0fad789a886f66'},
-	wolframalpha: {key:'U7L4VR-K3WJPLK6Y2'},
-	newsapi: {key:'f4504df34ba9432f80ff040a41736518'}
+const config = {
+    openweathermap: {key: 'f08bfa92a064d679ae0fad789a886f66'},
+    wolframalpha: {key: 'U7L4VR-K3WJPLK6Y2'},
+    newsapi: {key: 'f4504df34ba9432f80ff040a41736518'},
+    port: 4567,
+	zap: {ip: '192.168.178.172'}
 }
 
 module.exports = {
-	get:config
+    get: config
 }
