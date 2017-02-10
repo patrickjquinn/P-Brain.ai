c84af5bd0413d8cf15671f8f9f3f076cc691d31f
Tim Stableford <tim.stableford@gmail.com>
2017-02-10 09:11:05 +0000

    Added remind me skill

---
+++ skills/remind_me/index.js
@@ -0,0 +1,18 @@
+function hard_rule(query, breakdown) {
+    return query.trim().startsWith("remind me to");
+}
+
+function * reminder_resp(query) {
+    query = query.replace("remind me to", "").trim();
+    return {text: `It's time to ${query}.`}
+}
+
+const examples = () => (
+    ['Remind me to take out the bins.']
+)
+
+module.exports = {
+    get: reminder_resp,
+    hard_rule,
+    examples
+}
