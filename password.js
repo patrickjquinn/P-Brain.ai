e99c6b2e52063fd2ffdcddc4ed1d70dab8e5dc39
Tim Stableford <tim.stableford@gmail.com>
2017-02-14 15:18:58 +0000

    Updated password.js to new db

--- password.js
+++ password.js
@@ -1,7 +1,22 @@
-const md5 = require('md5')
-const config = require('./config').get
-if (process.argv.length != 3) {
-    console.log("Usage: password.js <password>")
+global.db = require('./sqlite_db')
+const auth = require('./authentication')
+const co = require('co')
+if (process.argv.length != 4) {
+    console.log("Usage: password.js <username> <password>")
 } else {
-    console.log(md5(process.argv[2] + config.auth.static_salt).toUpperCase())
+    co(function * () {
+        console.log("Setting up database.")
+        yield global.db.setup('pbrain.db')
+        const user = {
+          username: process.argv[2],
+          password: yield auth.encryptPassword(process.argv[3])
+        }
+        console.log(user)
+        yield global.db.saveUser(user)
+        console.log("User saved")
+    }).catch(err => {
+        console.log(err)
+        throw err
+    })
 }
+
