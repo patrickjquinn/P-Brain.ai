4492f396ad0563153617e00fee6129ae4144b34a
Tim Stableford <tim.stableford@gmail.com>
2017-02-14 15:01:21 +0000

    Removed unused configs

--- config/index.js
+++ config/index.js
@@ -2,12 +2,7 @@ const config = {
     openweathermap: {key: 'f08bfa92a064d679ae0fad789a886f66'},
     wolframalpha: {key: 'U7L4VR-K3WJPLK6Y2'},
     newsapi: {key: 'f4504df34ba9432f80ff040a41736518'},
-    port: 4567,
-    zap: {ip: '192.168.178.172'},
-    auth: {
-        jwtSecret: 'random_secret_key',
-        static_salt: '_pbrain_auth'
-    }
+    zap: {ip: '192.168.178.172'}
 }
 
 module.exports = {
