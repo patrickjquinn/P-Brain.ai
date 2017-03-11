bc7720aa9dbd3bc6fc83ccfe05cc3213b6df0786
Tim Stableford <tim.stableford@gmail.com>
2017-03-11 10:51:51 +0000

    Improved Simon says skill
    Simon says now uses a hard rule for better classification and
    has a 20% chance of prepending 'Simon says:'.

--- skills/simonsays/index.js
+++ skills/simonsays/index.js
@@ -1,14 +1,18 @@
-const intent = () => ({
-	keywords: ['simon says', 'simon says qqqq'],
-	module: 'simonsays'
-});
+function hard_rule(query, breakdown) {
+	return query.startsWith('simon says')
+}
 
 function * resp(query) {
-	const resp = query.split('says ')[1];
+	let resp = query.split('says ')[1]
+	resp = resp.charAt(0).toUpperCase() + resp.slice(1) + '.'
+	// 20% chance to append 'Simon says'
+	if (Math.random() < 0.2) {
+		resp = `Simon says: ${resp}`
+	}
 	return resp;
 }
 
 module.exports = {
 	get: resp,
-	intent
+	hard_rule: hard_rule
 };
