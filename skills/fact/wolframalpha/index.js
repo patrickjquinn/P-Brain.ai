d2d1e7cb3ad3377bad2e2cf8d0c0ec14a849bf2a
Tim Stableford <tim.stableford@gmail.com>
2017-11-18 15:33:06 +0000

    Fixed incorrect use of wolfram api

--- skills/fact/wolframalpha/index.js
+++ skills/fact/wolframalpha/index.js
@@ -14,21 +14,9 @@ co(function * () {
     throw err
 })
 
-function * query_wrapper(query) {
-    return new Promise(function (resolve, reject) {
-        client.query(query, function(err, result) {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(result)
-            }
-        })
-    })
-}
-
 function * wlfra_resp(query) {
     try {
-        const result = yield(query_wrapper(query))
+        const result = yield(client.query(query))
         for (let i = 0; i < result.length; i++) {
             if (result[i].primary) {
                 return result[i].subpods[0].text
