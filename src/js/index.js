9f39af200df800bb7fe586e58ecdfc6c3f8d6616
tb100 <54515877+tb100@users.noreply.github.com>
2020-04-08 14:19:36 -0400

    Updated with hard coded H/W for images

--- src/js/index.js
+++ src/js/index.js
@@ -46,8 +46,8 @@ function push_response(msg, callback) {
 function push_yt_response(id) {
     $('.chat').append('<div style="border-radius: 5px !important;" class="bubble you"><div id="player_container"><div class="player" id="player' + id + '"></div></div></div>')
 }
-function push_image(url,height,width){
-    $('.chat').append('<div style="border-radius: 5px !important;height: '+(height+40)+'px;width: '+(width+40)+'px;" class="bubble you"><div><img src="'+url+'" height="'+height+'" width="'+width+'"></div></div></div>');
+function push_image(url){
+    $('.chat').append('<div style="border-radius: 5px !important;height:340px;width:540px;" class="bubble you"><div><img src="'+url+'" height="300" width="500'"></div></div></div>');
 }
 
 function push_timer_response(msg) {
