a8b1f791fb80a33cf60918c6fea3c405959b86dc
tb100 <54515877+tb100@users.noreply.github.com>
2020-04-08 08:58:28 -0400

    Updated the API key, and changed 'var' to 'let'.

--- skills/nasa/index.js
+++ skills/nasa/index.js
@@ -1,7 +1,16 @@
 const request = require('../../api/request');
 
-//put your nasa api key here:
-const key = '';
+async function setup() {
+    if ((await global.db.getSkillValue('nasa', 'apikey')) == null) {
+        console.log('Setting default API key for Nasa');
+        await global.db.setSkillValue(
+            'nasa',
+            'apikey',
+            //put your nasa api key here:
+            '',
+        );
+    }
+}
 
 const intent = () => ({
     keywords: ['show the picture of the day', "what's today picture", 'show me the picture'],
@@ -9,14 +18,17 @@ const intent = () => ({
 });
 
 async function nasa_resp() {
+    
+    const key = await global.db.getSkillValue('nasa', 'apikey');
+    
     const nasa_url = 'https://api.nasa.gov/planetary/apod?api_key='+key;
 
     let data = await request(nasa_url);
 
     data = JSON.parse(data.body);
   
-    var title = data.title + ' by ' + data.copyright;
-    var image = data.url;
+    let title = data.title + ' by ' + data.copyright;
+    let image = data.url;
 
     return { text: title, image: data.url};
 }
@@ -27,4 +39,5 @@ module.exports = {
     get: nasa_resp,
     intent,
     examples,
+    setup
 };
